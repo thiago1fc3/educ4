9ab9aea3ed2146b612c03b4bd30942c5870892a3
Thiago Felippe <thiago@lar.ifce.edu.br>
2020-11-25 10:38:24 -0300

    Segurança com Google

--- api/src/main/java/br/com/educ4/Hello.java
+++ api/src/main/java/br/com/educ4/Hello.java
@@ -2,6 +2,8 @@ package br.com.educ4;
 
 import br.com.educ4.core.ports.driver.WelcomePort;
 import br.com.educ4.core.userstory.WelcomeUS;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +17,10 @@ public class Hello {
     @GetMapping
     public String get(){
         return  welcomePort.greeting("Hello");
+    }
+
+    @GetMapping("me")
+    public OAuth2User getMe(@AuthenticationPrincipal OAuth2User user){
+        return user;
     }
 }
