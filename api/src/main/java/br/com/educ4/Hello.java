bfa7e52769bbb0be6641699329e27332ba59b8de
Thiago Felippe <thiago@lar.ifce.edu.br>
2020-11-25 10:20:44 -0300

    Integração da API com o CORE

--- api/src/main/java/br/com/educ4/Hello.java
+++ api/src/main/java/br/com/educ4/Hello.java
@@ -1,5 +1,7 @@
 package br.com.educ4;
 
+import br.com.educ4.core.ports.driver.WelcomePort;
+import br.com.educ4.core.userstory.WelcomeUS;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
+    private WelcomePort welcomePort = new WelcomeUS();
+
     @GetMapping
     public String get(){
-        return  "Hello";
+        return  welcomePort.greeting("Hello");
     }
 }
