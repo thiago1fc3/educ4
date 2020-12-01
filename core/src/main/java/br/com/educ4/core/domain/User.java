f4fb87c66567ef0fdccf297607f9b8570c2a8188
Thiago Felippe <thiago@lar.ifce.edu.br>
2020-12-01 10:50:14 -0300

    User: Equals and Hash Code

--- core/src/main/java/br/com/educ4/core/domain/User.java
+++ core/src/main/java/br/com/educ4/core/domain/User.java
@@ -1,10 +1,12 @@
 package br.com.educ4.core.domain;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @Builder
+@EqualsAndHashCode(of = "id")
 public class User {
     private final String id;
     private final String username;
