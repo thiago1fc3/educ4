6ccd2c54f73a9b081b89a143670c48b061904713
Thiago Felippe <thiago@lar.ifce.edu.br>
2021-05-04 21:13:12 -0300

    Main: correção no id de usuário

--- api/src/main/java/br/com/educ4/controller/users/UserController.java
+++ api/src/main/java/br/com/educ4/controller/users/UserController.java
@@ -5,7 +5,7 @@ import br.com.educ4.controller.users.request.PatchUserRequest;
 import br.com.educ4.controller.users.response.DefaultUserResponse;
 import br.com.educ4.core.domain.User;
 import br.com.educ4.core.ports.driver.user.EnableUserPort;
-import br.com.educ4.core.ports.driver.user.FindUserByIdPort;
+import br.com.educ4.core.ports.driver.user.FindUserByUsernamePort;
 import br.com.educ4.core.ports.driver.user.PatchUserPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +21,7 @@ public class UserController {
     private final AuthUser authUser;
     private final PatchUserPort patchUserPort;
     private final EnableUserPort enableUserPort;
-    private final FindUserByIdPort findUserByIdPort;
+    private final FindUserByUsernamePort findUserByUsernamePort;
 
     // TODO Pegar o id do usuário pelo token
     @PatchMapping("{id}")
@@ -46,6 +46,7 @@ public class UserController {
 
     @GetMapping("me")
     public User me() throws GeneralSecurityException, IOException {
-        return authUser.getUser();
+        var googleUser = authUser.getUser();
+        return findUserByUsernamePort.execute(googleUser.getUsername(), User.class);
     }
 }
--- security/src/main/java/br/com/educ4/AuthUser.java
+++ security/src/main/java/br/com/educ4/AuthUser.java
@@ -46,15 +46,14 @@ public class AuthUser implements AuthUserPort {
             var idToken = gVerifier.verify(token);
 
             if (idToken != null) {
+
                 var payload = idToken.getPayload();
-                var user = findUserByUsernamePort.execute(payload.getEmail(), User.class);
 
                 return User.builder()
-                        .id((String) user.getId())
-                        .picture((String) user.getPicture())
-                        .username(user.getUsername())
-                        .name((String) user.getName())
-                        .enabled(user.isEnabled())
+                        .picture((String) payload.get("picture"))
+                        .username(payload.getEmail())
+                        .name((String) payload.get("name"))
+                        .enabled((boolean) payload.get("email_verified"))
                         .build();
 
             }
